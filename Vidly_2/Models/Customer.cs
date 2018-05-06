78032b0ddce912a04565483997213e4cdf3ca38d
Jeffrey Johnson <jefjohms@gmail.com>
2018-05-06 14:15:35 -0700

    Added and populated MembershipType table.

--- Vidly_2/Models/Customer.cs
+++ Vidly_2/Models/Customer.cs
@@ -9,5 +9,7 @@ namespace Vidly_2.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
     }
 }
