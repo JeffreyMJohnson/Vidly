65f08ecc743b5412888df847768f7a8a53f3d9c8
Jeffrey Johnson <jefjohms@gmail.com>
2018-05-06 13:37:42 -0700

    Enable code-first workflow and create the database.

--- Vidly_2/Models/IdentityModels.cs
+++ Vidly_2/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.Migrations.Model;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -20,6 +21,9 @@ namespace Vidly_2.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
